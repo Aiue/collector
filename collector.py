65983f744a41e0fd3b70c16919304002e6cd6ace
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-03-22 12:11:13 +0100

    Add class 'Archives'

--- collector.py
+++ collector.py
@@ -3,6 +3,8 @@
 # Written by Jens Nilsson Sahlin for Linköping University 2024
 # Additional details will be available in README.md
 
+import time
+
 # Classes
 class Archive:
     def __init__(self, archiveID, indexPathsFile):
@@ -16,3 +18,16 @@ class Archive:
                 self.clusterIndex = RemoteFile(line)
                 i = line.rfind('cluster.idx')
                 self.indexPathsURI = line[0:i]
+
+class Archives:
+    def __init__(self):
+        self.archives = {}
+        self.lastUpdate = 0
+
+    def update(self):
+        if time.time() - self.lastUpdate < 86400:
+            return
+        index = RemoteFile(config.archive_host + config.archive_list_uri)
+        contents = index.read()
+        # TODO: Parse the html response (or catch errors).
+        self.lastUpdate = time.time()
