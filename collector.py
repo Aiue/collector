66ad552e78cd091d9105fa9ee084896712ad19b3
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-03-22 12:02:34 +0100

    Add class 'Archive'

--- collector.py
+++ collector.py
@@ -3,3 +3,16 @@
 # Written by Jens Nilsson Sahlin for Linköping University 2024
 # Additional details will be available in README.md
 
+# Classes
+class Archive:
+    def __init__(self, archiveID, indexPathsFile):
+        self.archiveID = archiveID
+        self.indexPathsFile = RemoteFile(indexPathsFile)
+
+    def updatePaths(self):
+        f = RemoteFile(indexPathsFile)
+        for line in f.read(): # TODO: Catch errors.
+            if line.endswith('cluster.idx'):
+                self.clusterIndex = RemoteFile(line)
+                i = line.rfind('cluster.idx')
+                self.indexPathsURI = line[0:i]
