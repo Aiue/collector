f244a6866ea3188212c8c20e043b596c7cc74eea
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-09-03 16:03:44 +0200

    Arithmetic fix.

--- collector.py
+++ collector.py
@@ -317,7 +317,7 @@ class RemoteFile:
         #logger.debug('Getting from %s', self.url)
         if (time.time() - self.requests['last']) < config.min_request_interval:
             logger.debug('Request limit reached, sleeping for %f seconds.', time.time() - self.requests['last'])
-            time.sleep(config.min_request_interval(time.time() - self.requests['last']))
+            time.sleep(config.min_request_interval - (time.time() - self.requests['last']))
 
         time_start = time.time()
         headers = None # Should not need to be initialized/emptied, but do it anyway.
