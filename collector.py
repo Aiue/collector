1268aeceae82b272e8ee48582d408745f84a041d
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-10-11 15:13:21 +0200

    Hopefully avoid sleeping for negative time intervals.

--- collector.py
+++ collector.py
@@ -374,10 +374,10 @@ class RemoteFile:
 
     def get(self):
         #logger.debug('Getting from %s', self.url)
-        if (time.time() - self.requests['last']) < config.min_request_interval:
-            sleep_time = config.min_request_interval - (time.time() - self.requests['last'])
-            logger.debug('Request limit reached, sleeping for %f seconds.', sleep_time)
-            time.sleep(sleep_time)
+        time_diff = time.time() - self.requests['last']
+        if (time_diff < config.min_request_interval:
+            logger.debug('Request limit reached, sleeping for %f seconds.', time_diff)
+            time.sleep(time_diff)
 
         time_start = time.time()
         headers = None # Should not need to be initialized/emptied, but do it anyway.
