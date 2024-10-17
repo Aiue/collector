72d04b136b58d90a6a01932e1d53a71bc851ce43
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-10-17 10:44:42 +0200

    Add verbosity.

--- find_unindexed_files.py
+++ find_unindexed_files.py
@@ -13,15 +13,18 @@ def main():
     for archive in Path(config.pywb_collection_dir).iterdir():
         archives.append(archive.name)
     print('%d files found.' % len(archives))
-    print('Comparing against pywb index... ', end='', flush=True)
     with Path(Path(config.pywb_collection_dir).parents[0], 'indexes', 'autoindex.cdxj').open('r') as f:
+        lineno = 0
         for line in f.read().splitlines():
+            lineno += 1
             _,_,info = line.split(' ', 2)
             filename = json.loads(info)['filename']
-            if filename in archives:
+            if filename in archives: # Not exactly the most optimal approach, but this tool will only be used sparingly.
                 archives.remove(filename)
             else:
                 missing_archives.append(filename)
+            print('Comparing against pywb index... %d' % lineno, end='\r', flush=True)
+        print('Comparing against pywb index... done', end='\r', flush=True)
 
     print('%d files missing from index' % len(archives), end='')
     if len(archives) > 0:
