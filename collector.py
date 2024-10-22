a95e1de30c1d7e4aa6de97b1100df63334b7d968
Jens Nilsson Sahlin <jens.nilsson.sahlin@liu.se>
2024-10-22 14:11:55 +0200

    Argument fix that actually makes sense.

--- collector.py
+++ collector.py
@@ -385,7 +385,7 @@ class RemoteFile:
             f.write(contents)
         if config.pywb_dir:
             logger.debug('Running %s/bin/wb-manager add %s %s', config.pywb_dir, config.collection_name, str(self.filename))
-            subprocess.run([config.pywb_dir + 'bin' + 'wb-manager', 'add', config.collection_name, str(self.filename)], env={'VIRTUAL_ENV': config.pywb_dir, 'PATH': '%s:%s' % (config.pywb_dir, os.getenv('PATH'))}, check=True)
+            subprocess.run([config.pywb_dir + '/bin/wb-manager', 'add', config.collection_name, str(self.filename)], env={'VIRTUAL_ENV': config.pywb_dir, 'PATH': '%s:%s' % (config.pywb_dir, os.getenv('PATH'))}, check=True)
         else:
             Path(config.tempdir, self.filename.name).rename(self.filename)
 
